9fe90cca822047d6f66ef355d1de35c6c167cc36
Michael Gilbertson <mgg9xv@virginia.edu>
2016-06-09 20:42:09 -0400

    Added two more jasmine tests to describe the menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,6 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
         it('have non-empty URLs', function(){
             allFeeds.forEach(function(feed){
                 expect(feed.url).not.toBe(undefined);
@@ -44,7 +43,6 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-
         it('have non-empty Names', function(){
             allFeeds.forEach(function(feed){
                 expect(feed.name).not.toBe(undefined);                
@@ -55,18 +53,37 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function(){
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden by default', function(){
+            var body = $('body');
+            expect(body.hasClass("menu-hidden")).toBe(true);
+        })
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+        it('is visible when the menu icon is clicked', function(){
+            var body, menu;
+            body = $('body');
+            menuIcon = $('.menu-icon-link');
+            expect(body.hasClass("menu-hidden")).toBe(true);
+            menuIcon.trigger('click');
+            expect(body.hasClass("menu-hidden")).toBe(false);
+            menuIcon.trigger('click');
+            expect(body.hasClass("menu-hidden")).toBe(true);
+        });
+          
+    });
+
+        
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
